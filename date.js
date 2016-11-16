266b47c616f14003438f2ca0a845e5950e3061cb
Joe <jyull1@ithaca.edu>
2016-11-15 22:43:40 -0500

    Added Game State Framework

---
+++ date.js
@@ -0,0 +1,13 @@
+var date = {
+	preload: function(){
+		game.load.image('background', '/assets/backgrounds/background_left_(v1).png');
+	},
+
+	create: function(){
+
+	},
+
+	update: function(){
+
+	}
+}
---
+++ game.js
@@ -0,0 +1,6 @@
+var gameWidth = 1280,
+	gameHeight = 720;
+
+var game = new Phaser.Game(gameWidth, gameHeight);
+
+game.state.add('date', date);
