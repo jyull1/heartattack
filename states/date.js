0a32664c2e4fce4d3e9775b405322cf344358dbf
Joe <jyull1@ithaca.edu>
2016-11-17 15:51:38 -0500

    Readded Files I Deleted :/

---
+++ states/date.js
@@ -0,0 +1,13 @@
+var date = {
+	preload: function(){
+		game.load.image('background', '/assets/backgrounds/final_bg_left.png');
+	},
+
+	create: function(){
+		this.bg = game.add.image(0, 0, 'background');
+	},
+
+	update: function(){
+
+	}
+}
